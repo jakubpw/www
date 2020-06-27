6bbc15f6eda423e18af12d049abb82af4f3c984a
jakubpw <jawalendowski@gmail.com>
2020-06-27 16:30:46 +0200

    krok 8

---
+++ Linia lotnicza/testy.js
@@ -0,0 +1,14 @@
+import { fib } from "./program";
+import { expect } from "chai";
+import "mocha";
+describe("Fibonacci", () => {
+    it("should equal 0 for call with 0", () => {
+        expect(fib(0)).to.equal(0);
+    });
+    it("should equal 1 for call with 1", () => {
+        expect(fib(1)).to.equal(1);
+    });
+    it("should equal 1 for call with 2", () => {
+        expect(fib(2)).to.equal(1);
+    });
+});
